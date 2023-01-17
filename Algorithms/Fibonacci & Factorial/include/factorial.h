86dc02155b5683928feddba232789a81da42b231
Giuseppe Acanfora <101001028+AcanforaGiuseppe@users.noreply.github.com>
2023-01-17 08:16:49 +0100

    Delete Algorithms/Fibonacci & Factorial directory

--- Algorithms/Fibonacci & Factorial/include/factorial.h
+++
@@ -1,7 +0,0 @@
-#ifndef FACTORIAL_H
-#define FACTORIAL_H
-
-unsigned int factorial(unsigned int n);
-unsigned int factorial_ite(unsigned int n);
-
-#endif
--- Algorithms/Fibonacci & Factorial/include/fibonacci.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-unsigned int fibonacci(unsigned int n);
-
-unsigned int fibonacci_optimized(unsigned int n);
-
-unsigned int fibonacci_ite(unsigned int n);
